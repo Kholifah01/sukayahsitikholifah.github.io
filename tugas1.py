01dce756b2df5dc63981c2bb7992f534383db843
Kholifah01 <120782454+Kholifah01@users.noreply.github.com>
2022-12-24 17:25:17 +0700

    Add files via upload

---
+++ tugas1.py
@@ -0,0 +1,8 @@
+from array import*
+arif=array('i',[10,20,30,40,50])
+print (arif[0])
+print (arif[2])
+print()
+formulasi = arif[0]+(arif[1]*arif[0])
+print(arif[0],"+",arif[1],"x",arif[0],"=",formulasi)
+print(arif[1],"+",arif[1],"x",arif[0],"=",arif[1]+(arif[1]*arif[0])
